503ea5263d110cebdadf3ca069cddc05481529a0
davidmolina2810 <davidmolina2810@gmail.com>
2020-11-25 00:03:06 -0600

    commit tree a115286816ef1f5a0fc4932a8dad3ece21f87166

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  tutorials.map(str => 
+    str.split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.substring(1))
+    .join(' ')
+    )
 }
